165ff262c47946cfa40e33fada5e054bc24fe75a
Yevhen Kudiukin <kudyakin@gmail.com>
2022-06-11 12:42:03 +0300

    new branche added

--- src/main/java/com/kudiukin/notebooks/util/config/NotebookMapper.java
+++ src/main/java/com/kudiukin/notebooks/util/config/NotebookMapper.java
@@ -2,13 +2,10 @@ package com.kudiukin.notebooks.util.config;
 
 import com.kudiukin.notebooks.domain.Notebook;
 import com.kudiukin.notebooks.dto.NotebookDto;
-import ma.glasnost.orika.CustomMapper;
-import ma.glasnost.orika.MappingContext;
 
-public class NotebookMapper extends CustomMapper<Notebook, NotebookDto> {
 
-    @Override
-    public void mapBtoA(NotebookDto dto, Notebook entity, MappingContext context) {
-        super.mapBtoA(dto, entity, context);
-    }
+@Mapstruct
+public interface NotebookMapper {
+
+
 }
